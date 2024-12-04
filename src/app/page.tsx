77fbc6d8c5cff063c5bb46b2718898d9c88ce64f
nibezo <nibezo.cs@gmail.com>
2024-12-04 22:52:52 +0400

    stage the prev version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,37 @@
 "use client";
 
 import { retrieveLaunchParams } from "@telegram-apps/sdk";
+import { useEffect, useState } from "react";
 
 export default function Me() {
   const { initDataRaw } = retrieveLaunchParams();
+  const [message, setMessage] = useState("loading..");
 
   console.log(typeof initDataRaw);
 
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  const response = fetch("http://127.0.0.1:3001/auth/login", {
-    method: "POST",
-    headers: myHeaders,
-    body: JSON.stringify({ initData: initDataRaw }),
-  });
+    fetch("http://127.0.0.1:3001/auth/login", {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({ initData: initDataRaw }),
+      credentials: "include",
+    });
+
+    fetch("http://127.0.0.1:3001/auth/me", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
   return (
-    <div className="text-sm">
+    <div className="text-sm" style={{ backgroundColor: "red" }}>
       <code>Welcome back </code>
-      <code className="font-mono font-bold">@{initDataRaw}</code>
+      <code className="font-mono font-bold">{message}</code>
     </div>
   );
 }
