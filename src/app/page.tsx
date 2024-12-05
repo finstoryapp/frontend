b9054c672f2cde962b1c001f5a8e87525e996f98
nibezo <nibezo.cs@gmail.com>
2024-12-05 17:23:37 +0400

    add fetch util

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { retrieveLaunchParams, parseInitData } from "@telegram-apps/sdk";
 import { useEffect, useState } from "react";
+import { fetchUtil } from "../utils/utilFetch"; // Import the utility function
 
 export default function Me() {
   const [name, setName] = useState<string | undefined>();
@@ -14,48 +15,31 @@ export default function Me() {
     setName(initData.user?.firstName);
     setId(initData.user?.id);
 
-    let url = "";
-    if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
-      url = "http://127.0.0.1:3001/";
-    } else {
-      url = String(process.env.NEXT_PUBLIC_BACKEND_HOST);
-    }
-    fetch(url + "auth/login", {
+    const apiUrl =
+      process.env.NEXT_PUBLIC_BACKEND_HOST || "http://127.0.0.1:3001/";
+
+    // Use fetchUtil for login request
+    fetchUtil("auth/login", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
       body: JSON.stringify({ initData: initDataRaw }),
-      credentials: "include",
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
       .then((data) => {
-        console.log("Success:", data);
+        console.log("Login Success:", data);
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error("Login Error:", error);
       });
-    fetch(url + "auth/me", {
+
+    // Use fetchUtil for the 'me' request
+    fetchUtil("auth/me", {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
       .then((data) => {
         setMessage(data["message"]);
-        console.log("Success:", data);
+        console.log("Me Success:", data);
+      })
+      .catch((error) => {
+        console.error("Me Error:", error);
       });
   }, []);
 
@@ -64,7 +48,7 @@ export default function Me() {
       className="text-sm"
       style={{ backgroundColor: "white", color: "black" }}
     >
-      <code>Welcome back, ваше имя: {} </code>
+      <code>Welcome back, ваше имя: </code>
       <code className="font-mono font-bold">{name ? name : ""}</code>
       <div>
         <code>Ваш id: </code>
