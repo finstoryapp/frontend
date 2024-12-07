6fa3fd95b852f932df2e52305ca0ed98b4fb0fd8
nibezo <nibezo.cs@gmail.com>
2024-12-07 13:43:30 +0400

    downgrade react + add stats placeholder

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,12 +1,22 @@
 "use client";
+import "@telegram-apps/telegram-ui/dist/styles.css";
+import { AppRoot, Placeholder } from "@telegram-apps/telegram-ui";
 
 export default function Statistics() {
   return (
-    <div
-      className="text-sm"
-      style={{ backgroundColor: "white", color: "black" }}
-    >
-      <code className="font-mono font-bold">Страница статистики</code>
+    <div className="text-sm" style={{ color: "black" }}>
+      <AppRoot>
+        <Placeholder
+          header="Тут будет статистика"
+          description="Добавляйте расходы и она появится"
+        >
+          <img
+            alt="Telegram sticker"
+            src="/gifs/statistics.gif"
+            style={{ display: "block", width: "144px", height: "144px" }}
+          />
+        </Placeholder>
+      </AppRoot>
     </div>
   );
 }
