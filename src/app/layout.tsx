278bf792a0b564970361743c79febc0d342b625c
nibezo <nibezo.cs@gmail.com>
2024-12-07 23:56:16 +0400

    no scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"
+        />
+      </head>
       <body>
         <header>
           <span>Finstor</span>
