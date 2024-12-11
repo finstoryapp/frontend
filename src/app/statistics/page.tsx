82027b711d973c621776915eb4cc8ba5857c0797
nibezo <nibezo.cs@gmail.com>
2024-12-11 16:34:15 +0400

    @telegram-apps/telegram-ui remove

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,23 +1,18 @@
 "use client";
 import "@telegram-apps/telegram-ui/dist/styles.css";
-import { AppRoot, Placeholder } from "@telegram-apps/telegram-ui";
 import Image from "next/image";
 
 export default function Statistics() {
   return (
     <div className="text-sm" style={{ color: "white" }}>
-      <AppRoot>
-        <Placeholder description="Учитывайте расходы, чтобы увидеть статистику">
-          {" "}
-          <Image
-            src="/gifs/statistics.gif"
-            alt="your-alt-text"
-            width={100}
-            height={100}
-            priority={false}
-          />
-        </Placeholder>
-      </AppRoot>
+      {" "}
+      <Image
+        src="/gifs/statistics.gif"
+        alt="your-alt-text"
+        width={100}
+        height={100}
+        priority={false}
+      />
     </div>
   );
 }
