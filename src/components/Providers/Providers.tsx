c44bcefb0d1239c261c8576621886c2ea5754d4b
nibezo <nibezo.cs@gmail.com>
2025-05-19 01:56:59 +0400

    posthog

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -5,15 +5,27 @@ import { persistor, store } from "../../store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      capture_pageview: true,
+    });
+  }, []);
+
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </PersistGate>
-    </Provider>
+    <PHProvider client={posthog}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </PersistGate>
+      </Provider>
+    </PHProvider>
   );
 }
